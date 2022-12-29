58c10df63b822399d103914bae11fa4229559c56
Mike <m.kuskov@innopolis.university>
2022-12-30 02:51:00 +0300

    Update `meme` command, add `memeru` command

--- bot/handlers/meme/commands.py
+++ bot/handlers/meme/commands.py
@@ -1,24 +1,45 @@
 import random
 
 import requests
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, \
+    InlineKeyboardMarkup
 from telegram.ext import CallbackContext
 
 
 def meme_cmd(update: Update, _context: CallbackContext):
     try:
-        rand_link = 'https://imgflip.com/ajax_img_flip'
-        meme_id = requests.get(rand_link).content.decode()[3:]
-        update.message.reply_photo(f'https://i.imgflip.com/{meme_id}.jpg')
+        if random.randint(0, 1):
+            rand_link = 'https://imgflip.com/ajax_img_flip'
+            meme_id = requests.get(rand_link).content.decode()[3:]
+            meme_link = f'https://i.imgflip.com/{meme_id}.jpg'
+            source_link = f'https://imgflip.com/i/{meme_id}'
+        else:
+            meme_id = random.randint(6, 19791)
+            meme_link = f'https://t.me/bestmemes/{meme_id}'
+            source_link = meme_link
+        update.message.reply_photo(meme_link, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('🔗', url=source_link)]]))
     except requests.exceptions.RequestException as exception:
         update.message.reply_text('Srry, smth went wrong(')
         raise exception
 
 
+ru_meme_channel_list = [
+    # ('https://t.me/am_devs/27754'),
+    ('https://t.me/beobanka', 2, 21818),
+    ('https://t.me/LaQeque', 5, 50781),
+    ('https://t.me/paperpublic', 7, 18639),
+
+]
+
+
+# TODO: Add next button and callback_data handles for it
+# InlineKeyboardButton('🔁', callback_data='MEMERU_NEXT')
 def memeru_cmd(update: Update, _context: CallbackContext):
     try:
-        meme_id = random.randint(2, 21818)
-        update.message.reply_photo(f'https://t.me/beobanka/{meme_id}')
+        channel_link, start, end = random.choice(ru_meme_channel_list)
+        meme_id = random.randint(start, end)
+        meme_link = f'{channel_link}/{meme_id}'
+        update.message.reply_photo(meme_link, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('🔗', url=meme_link)]]))
     except requests.exceptions.RequestException as exception:
         update.message.reply_text('Srry, smth went wrong(')
         raise exception
