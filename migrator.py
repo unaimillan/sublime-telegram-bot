2c6872a5f6e6cb863bc50429bed64daa9a30bf3c
Mike <m.kuskov@innopolis.university>
2023-07-09 04:43:11 +0300

    Upgrade migrator

--- migrator.py
+++ migrator.py
@@ -16,7 +16,7 @@ from bot.handlers.game.models import Game as RawGame
 load_dotenv()
 engine = create_engine(os.getenv("DATABASE_URL", "Error no db url provided"), echo=False)
 # raw_data = pickle.load(open("storage/data.bin", "rb"))
-raw_data = pickle.load(open("storage/data.bin.backup3", "rb"))
+raw_data = pickle.load(open("storage/data.bin.backup4", "rb"))
 logging.info("All data loaded, sql engine created")
 
 
@@ -49,21 +49,30 @@ def populate_tg_users() -> list[int]:
     return list(users)
 
 
-async def get_user_id_info(client, tg_ids: List[int]):
+async def get_user_id_info(client):
     # me = await client.get_me()
     # print(me.stringify())
-    for tg_id in tg_ids:
-        user = await client.get_entity(tg_id)
-        await asyncio.sleep(0.25)
-        with Session(engine) as session:
-            tguser = session.query(TGUser).filter(TGUser.tg_id == tg_id).one()
-            tguser.username = user.username
-            tguser.first_name = user.first_name
-            tguser.last_name = user.last_name
-            tguser.lang_code = user.lang_code
-            session.commit()
-            session.refresh(tguser)
-            print(tguser)
+    with Session(engine) as session:
+        games: List[Game] = session.query(Game).filter(Game.chat_id < 0).all()
+        for game in games:
+            try:
+                chat_users = await client.get_participants(game.chat_id)
+            except ValueError:
+                chat_users = []
+                print(f'Chat {game.chat_id} failed to retrieve, processing..')
+            print(f'Chat {game.chat_id} retrieved with {len(chat_users)} users')
+            await asyncio.sleep(2)
+            for chat_user in chat_users:
+                maybe_user = session.query(TGUser).filter_by(tg_id=chat_user.id).all()
+                for db_user in maybe_user:
+                    # tg_user = await client.get_entity(db_user.tg_id)
+                    db_user.username = chat_user.username
+                    db_user.first_name = chat_user.first_name or '<undefined>'
+                    db_user.last_name = chat_user.last_name
+                    db_user.lang_code = chat_user.lang_code or 'en'
+                    session.commit()
+                    session.refresh(db_user)
+                    print(db_user)
 
 
 def populate_game():
@@ -78,10 +87,12 @@ def populate_game():
             session.refresh(game)
             for player in raw_game.players:
                 game.players.append(session.query(TGUser).filter_by(tg_id=player).one())
+            session.commit()
             for year in raw_game.years:
-                for day, winner in raw_game.years[year].items():
-                    game_result = GameResult(game_id=game.id, year=year, day=day, winner_id=winner)
-                    session.add(game_result)
+                for day, winner_tg_id in raw_game.years[year].items():
+                    winner: TGUser = session.query(TGUser).filter_by(tg_id=winner_tg_id).one()
+                    winner.game_results.append(GameResult(game_id=game.id, year=year, day=day))
+                    session.add(winner)
             session.commit()
 
 
@@ -104,24 +115,33 @@ def populate_kv_items():
 
 
 if __name__ == '__main__':
-    # recreate_db()
+    recreate_db()
     # populate_tiktok_links()
-    # populate_kv_items()
-    # tg_user_ids = populate_tg_users()
-    # populate_game()
+    populate_kv_items()
+    tg_user_ids = populate_tg_users()
+    populate_game()
 
-    with Session(engine) as session:
-        tg_id = 123456
-        user: TGUser = session.query(TGUser).filter_by(tg_id=tg_id).one()
-        game: Game = session.query(Game).filter_by(id=2).one()
-        print(user.full_username())
-        print(user.games)
-        print(len(game.players))
+    # with Session(engine) as session:
+    #     tg_id = 123456
+    #     user: TGUser = session.query(TGUser).filter_by(tg_id=tg_id).one()
+    #     game: Game = session.query(Game).filter_by(id=1).one()
+    #     session.add(GameResult(game=game, winner_id=user.id, year=2020, day=1))
+    #     print(user.full_username())
+    #     print(user.games)
+    #     print(len(game.players))
+    #     stmt = select(TGUser, func.count(GameResult.winner_id).label('count')) \
+    #         .join(TGUser, GameResult.winner_id == TGUser.id) \
+    #         .filter(GameResult.game_id == 2, GameResult.year == 2023) \
+    #         .group_by(TGUser) \
+    #         .order_by(text('count DESC'))
+    #     db_results = session.exec(stmt).all()
+    #     print(*db_results, sep='\n')
 
-    # client = TelegramClient('test', int(os.environ['TG_API_ID']),
-    #                         os.environ['TG_API_HASH'])
+    # client = TelegramClient('sublimebothelper', int(os.environ['TG_API_ID']),
+    #                         os.environ['TG_API_HASH']).start(bot_token=os.environ['TELEGRAM_BOT_API_SECRET'])
+    #
     # with client:
-    #     client.loop.run_until_complete(get_user_id_info(client, [267726592]))
+    #     client.loop.run_until_complete(get_user_id_info(client))
 
     # connection = engine.raw_connection()
     # with open('dump.sql', 'w') as f:
